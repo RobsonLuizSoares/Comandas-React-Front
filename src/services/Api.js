8e710e8d733a28a19e39202a32a0624b94083bb5
RobsonLuizSoares <robsonptrainer@gmail.com>
2019-06-26 11:45:48 -0300

    add URL from API

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,3 +1,8 @@
 import axios from 'axios'
+const REACT_APP_API_URL = 'http://localhost:3001'
 
-const api = axios.create({ baseURL: ''})
+const api = axios.create({
+   baseURL: process.env.REACT_APP_API_URL
+  })
+
+export default api
